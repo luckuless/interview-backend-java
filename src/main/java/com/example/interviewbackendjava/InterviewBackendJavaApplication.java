9a85cd2dbd2c4fde410a127899d8883ed0e5b2ab
luckuless <luckuless.rambojun@icloud.com>
2024-04-23 09:12:04 +0200

    repo setup

---
+++ src/main/java/com/example/interviewbackendjava/InterviewBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.interviewbackendjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewBackendJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewBackendJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/interviewbackendjava/InterviewBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interviewbackendjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewBackendJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
